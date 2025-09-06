950a93345710e5c098eece27d9a1e9e55f4e6dfe
godabharat1312 <godabharatkumar@gmail.com>
2025-09-06 15:54:21 +0530

    functions

---
+++ funsctions.js
@@ -0,0 +1,18 @@
+function greet(){
+    console.log("welcome to jntu")
+}
+greet()
+
+const multiply=function(x,y){
+    return x*y; 
+}
+console.log(multiply(5,3))
+
+const square =(x)=>x*x;
+console.log(square(5))
+
+const subtract = (x,y)=>{
+    return x-y;
+}
+console.log(20,15)
+console.log(subtract(20,15))
