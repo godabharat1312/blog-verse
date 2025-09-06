d3747b56e8e469b955b10bc8987b5399fad4520b
godabharat1312 <godabharatkumar@gmail.com>
2025-09-06 12:33:38 +0530

    learned js

--- sample.js
+++ sample.js
@@ -1,1 +1,24 @@
 console.log("this is sample")
+let name = "bharat";
+console.log(name)
+let num=18;
+console.log(16)
+console.log(typeof name)
+console.log(typeof num )
+let x;
+console.log(x)
+console.log(typeof x)
+let y=null;
+console.log(y)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"bharat",
+    age:19
+}
+console.log(person.name)
+console.log(typeof person)
